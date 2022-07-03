9bc5c963db5c58502f07934f484f50cd0ca5e994
saurami <saurabhm@proton.me>
2022-07-03 13:30:26 -0700

    added failing test

--- hello.rs
+++ hello.rs
@@ -9,4 +9,10 @@ fn main() {
 #[test]
 fn test_message() {
     assert_eq!(message(), "Hello, World!")
+}    
+
+#[test]
+#[should_panic]
+fn test_incorrect_message() {
+    assert!(message() == "Hello World")
 }
