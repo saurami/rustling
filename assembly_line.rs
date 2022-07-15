2779de5787f33a86ea56437b8fc30175005909cb
saurami <saurabhm@proton.me>
2022-07-15 01:20:45 -0700

    fixed compile errors

--- assembly_line.rs
+++ assembly_line.rs
@@ -1,5 +1,5 @@
 fn production_rate_per_hour(speed: u8) -> f64 {
-	let production = speed as f64 * 221
+	let production = speed as f64 * 221.0;
 	if speed > -1 && speed <= 4 {
 		production * 1.00
 	} else if speed > 4 && speed <= 8 {
@@ -12,7 +12,7 @@ fn production_rate_per_hour(speed: u8) -> f64 {
 }
 
 fn working_items_per_minute(speed: u8) -> u32 {
-	production_rate_per_hour(speed) / 60
+	production_rate_per_hour(speed) as u32 / 60
 }
 
 fn main() {
@@ -23,7 +23,7 @@ fn main() {
 
 #[test]
 fn test_production_with_no_faulty_cars() {
-	assert_eq!(production_rate_per_hour(4) == 884)
+	assert_eq!(production_rate_per_hour(4) == 884.0)
 }
 
 #[test]
