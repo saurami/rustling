0d86e37372c9e478922badfa8e12a4b29e8b670d
saurami <saurabhm@proton.me>
2022-07-16 14:30:16 -0700

    enums and match with functions

---
+++ log_levels.rs
@@ -0,0 +1,61 @@
+enum LogLevel {
+	Info,
+	Warning,
+	Error
+}
+
+fn emit_log(message_type: LogLevel, content: &str) -> String {
+	match message_type {
+		LogLevel::Info => info(content),
+		LogLevel::Warning => warning(content),
+		LogLevel::Error => error(content)
+	}
+}
+
+fn info(message: &str) -> String {
+	return format!("[INFO]: {}", message)
+}
+
+fn warning(message: &str) -> String {
+	return format!("[WARNING]: {}", message)
+}
+
+fn error(message: &str) -> String {
+	return format!("[ERROR]: {}", message)
+}
+
+fn main() {
+	emit_log(LogLevel::Info, "Timezone changed");
+	emit_log(LogLevel::Error, "Stack Overflow");
+}
+
+
+#[test]
+fn test_emit_info_log() {
+	assert_eq!(emit_log(LogLevel::Info, "Timezone changed"), "[INFO]: Timezone changed")
+}
+
+#[test]
+fn test_emit_warning_log() {
+	assert_eq!(emit_log(LogLevel::Warning, "Timezone not set"), "[WARNING]: Timezone not set")
+}
+
+#[test]
+fn test_emit_error_log() {
+	assert_eq!(emit_log(LogLevel::Error, "Stack Overflow"), "[ERROR]: Stack Overflow")
+}
+
+#[test]
+fn test_info() {
+	assert!(info("Timezone changed") == "[INFO]: Timezone changed")
+}
+
+#[test]
+fn test_warning() {
+	assert!(warning("Timezone not set") == "[WARNING]: Timezone not set")
+}
+
+#[test]
+fn test_error() {
+	assert!(error("Stack Overflow") == "[ERROR]: Stack Overflow")
+}
