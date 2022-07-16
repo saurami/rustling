30ae0b6102af3e452b330c72eb3c9710ccbadfe0
saurami <saurabhm@proton.me>
2022-07-16 11:42:59 -0700

    different functions for log message

--- log_levels.rs
+++ log_levels.rs
@@ -6,15 +6,39 @@ enum LogLevel {
 fn emit_log(message_type: LogLevel) {
 	match message_type {
 		LogLevel::Info => {
-			println!("[INFO] Timezone changed");
+			println!("{}", info_message());
 		},
 		LogLevel::Error => {
-			println!("[ERROR] Stack Overflow");
+			println!("{}", error_message());
 		}
 	}
+}
+
+fn info_message() -> String {
+	String::from("[INFO] Timezone changed")
+}
+
+fn error_message() -> String {
+	String::from("[ERROR] Stack Overflow")
 }
 
 fn main() {
 	emit_log(LogLevel::Info);
 	emit_log(LogLevel::Error);
 }
+
+
+// #[test]
+// fn test_info_message() {
+// 	let message = LogLevel::Info;
+// }
+
+// #[test]
+// fn test_warning() {
+
+// }
+
+// #[test]
+// fn test_error() {
+
+// }
